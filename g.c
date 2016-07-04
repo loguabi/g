0284112c915ecc0a15e78c169911c9d3a4059d81
logitha <loga848.456@gmail.com>
2016-07-04 13:41:23 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int i,a,b,c,d,e,abbe;
+printf("Enter the integer number:");
+scanf("%d",&i);
+for(i=1;i<=26;i++);
+{
+if(i==1)
+{
+printf("%d",a);
+}
+else if(i==2)
+{
+printf("%d",b);
+}
+else if(i==3)
+{
+printf("%d",c);
+}
+else if(i==4)
+{
+printf("%d",d);
+}
+else if(i==5)
+{
+printf("%d",e);
+}
+else if(i==1225)
+{
+printf("%d",abbe);
+
+}
+}
+return 0;
+}
